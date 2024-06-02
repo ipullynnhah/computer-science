3033854d56e85a0d30dd7e0ec24cfe7dcd8a8d48
Paula Rodrigues <ipullynnhah@gmail.com>
2024-06-02 01:18:19 -0300

    :octocat: project 3 - magic 8-ball (bonus)

--- projects/project-03/magic-8-ball/magic8.py
+++ projects/project-03/magic-8-ball/magic8.py
@@ -3,29 +3,60 @@ import random
 name = "Paula"
 question = "Will I have an exciting adventure this year?"
 answer = ""
-random_number = random.randint(1, 9)
+random_number = random.randint(1, 20)
 # print(random_number)
 
 if random_number == 1:
-    answer = "Yes - definitely"
+    answer = "It is certain"
 elif random_number == 2:
     answer = "It is decidedly so"
 elif random_number == 3:
     answer = "Without a doubt"
 elif random_number == 4:
-    answer = "Reply hazy, try again"
+    answer = "Yes definitely"
 elif random_number == 5:
-    answer = "Ask again later"
+    answer = "You may rely on it"
 elif random_number == 6:
-    answer = "Better not tell you now"
+    answer = "As I see it, yes"
 elif random_number == 7:
-    answer = "My sources say no"
+    answer = "Most likely"
 elif random_number == 8:
-    answer = "Outlook not so good"
+    answer = "Outlook good"
 elif random_number == 9:
+    answer = "Yes"
+elif random_number == 10:
+    answer = "Signs point to yes"
+elif random_number == 11:
+    answer = "Reply hazy, try again"
+elif random_number == 12:
+    answer = "Ask again later"
+elif random_number == 13:
+    answer = "Better not tell you now"
+elif random_number == 14:
+    answer = "Cannot predict now"
+elif random_number == 15:
+    answer = "Concentrate and ask again"
+elif random_number == 16:
+    answer = "Don't count on it"
+elif random_number == 17:
+    answer = "My reply is no"
+elif random_number == 18:
+    answer = "My sources say no"
+elif random_number == 19:
+    answer = "Outlook not so good"
+elif random_number == 20:
     answer = "Very doubtful"
 else:
     answer = "Error"
 
-print(name, "asks:", question)
-print("Magic 8-Ball's answer:", answer)
+
+if question == "":
+    print(
+        "Magic 8-Ball cannot provide a fortune, otherwise, the fabric of reality is at risk!"
+    )
+else:
+    if name == "":
+        print("Question:", question)
+    else:
+        print(name, "asks:", question)
+    print("Magic 8-Ball's answer:", answer)
